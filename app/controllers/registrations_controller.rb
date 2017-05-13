fd288975384a5628f8c69fca53a4f11af88d7301
root <root@box.nobulldomain.com>
2017-05-13 04:48:30 -0500

    first commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,62 @@
+class RegistrationsController < ApplicationController
+  before_action :set_registration, only: [:show, :edit, :update, :destroy]
+
+  # GET /registrations
+  def index
+    @registrations = Registration.all
+  end
+
+  # GET /registrations/1
+  def show
+  end
+
+  # GET /registrations/new
+  def new
+    @registration = Registration.new
+    @course = Course.find_by id: params["course_id"]
+  end
+
+  # POST /registrations
+#  def create
+#    @registration = Registration.new(registration_params)
+
+#    respond_to do |format|
+#      if @registration.save
+#        format.html { redirect_to @registration, notice: 'Registration was successfully created.' }
+#        format.json { render :show, status: :created, location: @registration }
+#      else
+#        format.html { render :new }
+#        format.json { render json: @registration.errors, status: :unprocessable_entity }
+#      end
+#    end
+#
+
+def create
+    @registration = Registration.new registration_params.merge(email: stripe_params["stripeEmail"],
+                                                               card_token: stripe_params["stripeToken"])
+    raise "Please, check registration errors" unless @registration.valid?
+    @registration.process_payment
+    @registration.save
+    redirect_to @registration, notice: 'Registration was successfully created.'
+  rescue e
+    flash[:error] = e.message
+    render :new
+  end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_registration
+      @registration = Registration.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def registration_params
+      params.require(:registration).permit(:course_id, :full_name, :company, :email, :telephone)
+    end
+
+     def stripe_params
+      params.permit :stripeEmail, :stripeToken
+    end
+end
